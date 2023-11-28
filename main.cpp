35c5f5ee27c444304759057b0ebd418482ce0fc0
OmarZL05 <omar2zabala@gmail.com>
2023-11-28 17:21:50 -0400

    Arreglando lineas mal colocadas

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    String nombre;
+    int edad;
+
+    cout << "Indique su nombre: "
+    cin >> nombre;
+    cout << "Indique su edad: "
+    cin >> edad;
+    
+
+    return 0;
+}
