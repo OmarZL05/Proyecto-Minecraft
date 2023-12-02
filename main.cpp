1a3a3df1fd6f4939bc0e0f22e675e3de74d688ae
OmarZL05 <omar2zabala@gmail.com>
2023-12-02 12:30:31 -0400

    Implementación de la sección 6

--- main.cpp
+++ main.cpp
@@ -3,13 +3,24 @@ using namespace std;
 
 int main() {
 
-    string nombre, twitch, youtube, lugarRespawn, itemInicial, items, nombreMayorCantDiamante;
+    string nombre, twitch, youtube, lugarRespawn, itemInicial, items, streamerMasDiamantes;
     int edad, subs, subsTwitch, subsYoutube, horaInicio, horaFinal, horasEnStream;
     int cantOro, cantHierro, cantDiamante, cantPicosDiamantes, mayorCantDiamante;
-    int totalOro, totalHierro;
-    int i;
+    int totalOro, totalHierro, i;
+    float promHierro;
     int contMazmorras, contValle, contCabo, contVilla, cantStreamers;
     char seguirEntrevistando;
+
+    // Sección de inicializacion de variables.
+    cantStreamers = 0;
+    contMazmorras = 0;
+    contValle = 0;
+    contVilla = 0;
+    contCabo = 0;
+    totalOro = 0;
+    totalHierro = 0;
+    mayorCantDiamante = 0;
+    streamerMasDiamantes = "Ningun jugador";
 
     do{
         // Sección 1: Encuesta al jugador.
@@ -113,11 +124,35 @@ int main() {
 
         cout << "\nDesea entrevistar a alguien más ? [s/n]: ";
         cin >> seguirEntrevistando;
-        cout << " ";
+        cout << " " << endl;
+
+        // Sin sección
+        cantStreamers = cantStreamers + 1;
+        totalOro = totalOro + cantOro;
+        totalHierro = totalHierro + cantHierro;
 
     } while(seguirEntrevistando == 's');
     
-    // Sección 6: Final del proyecto, mostrar datos generales en pantalla. 
+    promHierro = totalHierro / cantStreamers;
 
+    // Sección 6: Final del proyecto, mostrar datos generales en pantalla. 
+    /*
+    Cantidad de streamers registrados.
+    Nombre del streamer que más diamantes recibió.
+    Total en Oro recibido.
+    Promedio en hierro recibido.
+    Cantidad de jugadores en el valle alegre.
+    Porcentaje de jugadores en las mazmorras de la torre oscura.
+    Diferencia en porcentaje de jugadores en Playa de Cabo Blanco y Poblado de Villa Chica: Deberá ser expresado así "En Playa de Cabo Blanco hay un 25% de jugadores más que en Villa Chica" o "En Playa de Cabo Blanco hay un 25% de jugadores menos que en Villa Chica" según corresponda.
+    Cantidad de jugadores que pidieron "Pico de Diamante"
+    */
+
+    cout << "\nCantidad de streamers registrados: " << cantStreamers << endl;
+    cout << "Streamer con más diamantes: " << streamerMasDiamantes << endl;
+    cout << "Total en oro: " << totalOro << endl;
+    cout << "Promedio de hierro: " << promHierro;
+    cout << "Cantidad de jugadores que elegieron Pico de Diamante, como item inicial: " << cantPicosDiamantes;
+    cout << "Cantidad de jugadores en el Valle Alegre: " << contValle << endl;
+    
     return 0;
 }
