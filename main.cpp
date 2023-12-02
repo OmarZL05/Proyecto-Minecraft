d57e1986ffaeb668ea25e2a8b637c36990a619d8
OmarZL05 <omar2zabala@gmail.com>
2023-12-01 21:55:13 -0400

    Se añadieron cositas

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
 
-    string nombre, twitch, youtube, lugarRespawn, nombreMayorCantDiamante;
+    string nombre, twitch, youtube, lugarRespawn, itemInicial, nombreMayorCantDiamante;
     int edad, subs, subsTwitch, subsYoutube, horaInicio, horaFinal, horasEnStream;
-    int cantOro, cantHierro, cantDiamante, mayorCantDiamante;
+    int cantOro, cantHierro, cantDiamante, cantPicosDiamantes, mayorCantDiamante;
     int totalOro, totalHierro;
+    int i;
     int contMazmorras, contValle, contCabo, contVilla, cantStreamers;
-    bool seguirEntrevistando;
+    char seguirEntrevistando;
 
     do{
         cout << "Indique su nombre: ";
@@ -28,10 +29,33 @@ int main() {
         cout << "Cuanto tiempo vas a estar en stream: ";
         cin >> horasEnStream;
 
-        cantOro = 25;
         subs = subsTwitch+subsYoutube;
-        cantOro = subs*0.5;
-    
-    } while(seguirEntrevistando);
+
+        cantOro = 25+subs*5/100;
+        cout << "Se te han asignado [" << cantOro << "] de oro" << endl;
+
+        do {
+            cout << "Cuantas unidades de hierro desea adquirir, son 10 hierro x 1 oro, [solo multiplos de 10]: ";
+            cin >> cantHierro;
+        } while ( (cantHierro % 10 != 0) && cantHierro < 10 && cantHierro/10 < cantOro);
+
+        cantOro = cantOro - cantHierro/10;
+
+        cantPicosDiamantes = 0;
+        for(i = 1; i <= 3; i++) {
+            cout << "Indique el item inicial [" << i << "/3]: ";
+            cin >> itemInicial;
+            if(itemInicial == "Pico de Diamante") {
+                cantPicosDiamantes = cantPicosDiamantes + 1;
+            }
+        }
+
+        cout << "Debug PC:" << cantPicosDiamantes << " Hierro:" << cantHierro << "  Oro: " << cantOro;
+
+        cout << "\nDesea entrevistar a alguien más ? [s/n]: ";
+        cin >> seguirEntrevistando;
+        cout << " ";
+
+    } while(seguirEntrevistando == 's');
     return 0;
 }
